20a3823363b2fb0363652e2c57aeeececf01652f
Madhava Reddy <madhava.aasam@gmail.com>
2021-04-02 11:58:07 +0530

    babel setup

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var http = require('http');
+import { createServer } from 'http';
 
-var server = http.createServer(function(req, res) {
+var server = createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "application/json"})
 
     const url = req.url;
-    console.log('url ----', url);
 
     if(url == '/employees') {
         const employees = [{id: 101, name: 'Paul bryant'}, {id: 102, name: 'Ning'}]
-        //  res.write(employees);
+        
         res.end(JSON.stringify(employees));
+    } else {
+        res.end('Illegal request')
     }
 });
-
-
-
 
 var port = process.env.PORT || 3002;
 server.listen(port, '', function(){
